f6c1189398b56449b4235172a8593c7de581fd11
imagoodman <i92335836@163.com>
2018-11-20 22:19:18 +0800

    用朴素贝叶斯进行了预测+简单调参

--- nb.py
+++ nb.py
@@ -11,6 +11,30 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.externals import joblib
 import sklearn.metrics
 import time
+from math import radians, atan, tan, sin, acos, cos
+
+
+def get_distance(latA, lonA, latB, lonB):
+    ra = 6378140  # radius of equator: meter
+    rb = 6356755  # radius of polar: meter
+    flatten = (ra - rb) / ra  # Partial rate of the earth
+    # change angle to radians
+    radLatA = radians(latA)
+    radLonA = radians(lonA)
+    radLatB = radians(latB)
+    radLonB = radians(lonB)
+
+    try:
+        pA = atan(rb / ra * tan(radLatA))
+        pB = atan(rb / ra * tan(radLatB))
+        x = acos(sin(pA) * sin(pB) + cos(pA) * cos(pB) * cos(radLonA - radLonB))
+        c1 = (sin(x) - x) * (sin(pA) + sin(pB)) ** 2 / cos(x / 2) ** 2
+        c2 = (sin(x) + x) * (sin(pA) - sin(pB)) ** 2 / sin(x / 2) ** 2
+        dr = flatten / 8 * (c1 - c2)
+        distance = ra * (x + dr)
+        return distance  # meter
+    except:
+        return 0.0000001
 
 
 def analyze_workday(date):
@@ -123,7 +147,7 @@ print('data shape is :',data.shape)
 # 初步用贝叶斯进行train的部分集进行训练
 nb = NBclassifier()
 n_1 = 10000
-n_2 = 100000
+n_2 = 10000
 x = data.loc[0:80,['week', 'hour', 'start_lat', 'start_lon']]
 lat = data.loc[0:80,'end_lat']
 x_pre = data.loc[81:127,['week', 'hour', 'start_lat', 'start_lon']]
